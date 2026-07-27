81021311b5d465a335ca7319996669f2c5a9279c
sayehava <93396701+sayehava@users.noreply.github.com>
2026-07-27 14:19:07 +0200

    feat: add remote_prints label string

--- src/pridge_client/webui/strings.js
+++ src/pridge_client/webui/strings.js
@@ -104,6 +104,7 @@ window.PridgeStrings = Object.freeze({
   done: "Done",
   working: "Working…",
   test_print: "Test Print",
+  remote_prints: "Remote",
   test_print_driver_only: "Test Print is available in System Driver mode.",
   test_print_failed: "Could not submit the test page.",
   test_print_submitted: "Test page submitted to the printer.",
